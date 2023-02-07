e95d4e78a08b43c6ce8cd8ad68ef815b2b6e8d79
Ambassador Chinatu <123711532+ambassadorchinatu@users.noreply.github.com>
2023-02-07 10:34:50 +0100

    Latest update

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,8 +10,8 @@ import { Svg, Path, Circle } from 'react-native-svg';
 import Animated, { useAnimatedProps, withSpring, useAnimatedStyle, useSharedValue, withDelay, withTiming } from 'react-native-reanimated';
 
 const {width, height} = Dimensions.get('screen');
-const Circle_Len = 1000;
-const Radius = Circle_Len / (2 * Math.PI);
+const Circle_Length = 1000;
+const Radius = Circle_Length / (2 * Math.PI);
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 const AnimatedPath = Animated.createAnimatedComponent(Path);
@@ -20,38 +20,43 @@ const App = () => {
   const [loading, setLoading] = useState(false);
 
 const progressCircle = useSharedValue(1);
-const markScale = useSharedValue(1);
-const circleOpacity = useSharedValue(0);
-const markOpacity = useSharedValue(0);
+const tickScale = useSharedValue(1);
+const bckgrdOpacity = useSharedValue(0);
+const tickOpacity = useSharedValue(0);
+
 const animatedProps= useAnimatedProps(() => ({
-  strokeDashoffset: Circle_Len * progressCircle.value,
+  strokeDashoffset: Circle_Length * progressCircle.value,
 }))
+
+useEffect(() => {
+  progressCircle.value = withTiming(0, {duration: 2000})
+}, [])
 
 const mountAnimation = () => {
   progressCircle.value = withTiming(0, {duration: 2000})
-  markScale.value = withDelay(1000, withSpring(80));
-  circleOpacity.value = withDelay(1000, withTiming(1));
-  markOpacity.value = withDelay(1500, withTiming(1, {duration: 500}) );
+  tickScale.value = withDelay(1000, withSpring(80));
+  bckgrdOpacity.value = withDelay(1000, withTiming(1));
+  tickOpacity.value = withDelay(1500, withTiming(1, {duration: 500}) );
   setLoading(true)
   setTimeout(() => {
     setLoading(false)
     progressCircle.value = withTiming(1, {duration: 500})
-    markScale.value = withDelay(500, withSpring(0));
-    circleOpacity.value = withDelay(500, withTiming(0));
-    markOpacity.value = withDelay(500, withTiming(0, {duration: 500}) );
+    tickScale.value = withDelay(500, withSpring(0));
+    bckgrdOpacity.value = withDelay(500, withTiming(0));
+    tickOpacity.value = withDelay(500, withTiming(0, {duration: 500}) );
     }, 3000);
 };
 
-const reanimatedStyle = useAnimatedStyle(() => {
+const bckgrdAnimatedStyle = useAnimatedStyle(() => {
   return {
-    opacity: circleOpacity.value,
-    transform: [{scale: markScale.value}]
+    opacity: bckgrdOpacity.value,
+    transform: [{scale: tickScale.value}]
   }
 })
 
 const tickAnimatedStyle = useAnimatedStyle(() => {
   return {
-    opacity: markOpacity.value,
+    opacity: tickOpacity.value,
   }
 });
 
@@ -76,43 +81,41 @@ const tickAnimatedStyle = useAnimatedStyle(() => {
         stroke="#82CD47"
         strokeWidth={15}
         fill="transparent"
-        strokeDasharray={Circle_Len}
+        strokeDasharray={Circle_Length}
         animatedProps={animatedProps}
         strokeLinecap="round"
-        />
+        />    
       </Svg>
       
-      <Animated.View 
-        style={[{
-          height: 3,
-          position: 'absolute',
-          width: 3,
-          bottom: 340,
-          backgroundColor: '#54B435',
-          borderRadius: 150,
-        },
-        reanimatedStyle,
-        ]}/>
-      <View 
-        style={{
-          height: 110,
-          position: 'absolute',
-          width: 110,
-          bottom: 290,
-          backgroundColor: 'transparent',
-          transform: [{scale: 2}]
-        }}>
-        <Svg viewBox='0 0 40 40'>
-        <AnimatedPath
-        d="M12.5 20l5 5 9-9"
-        stroke={"#fff"}
-        strokeWidth={3}
-        fill="#54B435"
-        style={tickAnimatedStyle}
-        strokeLinecap="round"
-        />
-      </Svg>
-        </View>
+        <Animated.View 
+          style={[{
+            height: 3,
+            position: 'absolute',
+            width: 3,
+            bottom: 340,
+            backgroundColor: '#54B435',
+            borderRadius: 150,
+          },
+          bckgrdAnimatedStyle,
+          ]}/>
+          <Svg viewBox='0 0 40 40' 
+          style={{
+            height: 110,
+            position: 'absolute',
+            width: 110,
+            bottom: 290,
+            backgroundColor: 'transparent',
+            transform: [{scale: 2}] 
+          }}>
+          <AnimatedPath
+          d="M12.5 20l5 5 9-9"
+          stroke={"#fff"}
+          strokeWidth={3}
+          fill="#54B435"
+          style={tickAnimatedStyle}
+          strokeLinecap="round"
+          />
+        </Svg>
        </>
       : 
       null
